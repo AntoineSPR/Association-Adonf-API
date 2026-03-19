68219e5c266d5150099d96720229e2ddb2d9f41e
AntoineSPR <antoine.simper@gmail.com>
2026-03-19 02:06:20 +0100

    Add migrations for PageContent

---
+++ Association-Adonf-API/Migrations/20260319005640_AddPageContent.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AssociationAdonfAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPageContent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PageContents",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Slug = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Content = table.Column<JsonDocument>(type: "jsonb", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    IsArchived = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PageContents", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("12ccaa16-0d50-491e-8157-ec1b133cf120"),
+                column: "ConcurrencyStamp",
+                value: "676ecb79-5ee2-42ec-93e7-1e3d3c12af33");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("63a2a3ac-442e-4e4c-ad91-1443122b5a6a"),
+                column: "ConcurrencyStamp",
+                value: "79893d7d-4490-4459-a43b-e889a0ef86ae");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PageContents");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("12ccaa16-0d50-491e-8157-ec1b133cf120"),
+                column: "ConcurrencyStamp",
+                value: "86d24214-9e27-4054-a725-6863a365d625");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("63a2a3ac-442e-4e4c-ad91-1443122b5a6a"),
+                column: "ConcurrencyStamp",
+                value: "f6e63bff-3d6f-4e41-9aac-ddec0102f3f1");
+        }
+    }
+}
